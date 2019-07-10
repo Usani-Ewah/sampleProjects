4b25850972cb31a6d8331c1c8b25a660ff892a48
Usani-Ewah <33046103+Usani-Ewah@users.noreply.github.com>
2019-07-10 09:22:28 -0500

    Fully functioning e-commerce app

---
+++ Angular projects/src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'iPhone XL',
+    price: 799,
+    description: 'A large iphone with one of the best chips and screens out on the market'
+  },
+  {
+    name: 'iPhone',
+    price: 699,
+    description: 'A great phone with one of the best cameras and a standard size for your viewing pleasure'
+  },
+  {
+    name: 'Pixel 3',
+    price: 299,
+    description: 'An all around complete phone with the ability to change the way phones are used at an affordable price'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
